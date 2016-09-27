fd7377738e42a14cc521c528259267d625bcdb7e
pawulon <pawelryndak@gmail.com>
2016-09-27 22:40:49 +0200

    Modified FT to check lists for two different users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -29,6 +29,8 @@ class NewVisitorTest(LiveServerTestCase):
         
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        user_list_url = self.browser.current_url
+        self.assertRegex(user_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -36,5 +38,25 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')       
+        
+        self.browser.quit()
+        self.browser = webdriver.Chrome(chromedriver_path)
+        
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        
+        second_user_list_url = self.browser.current_url
+        self.assertRegex(second_user_list_url, '/lists/.+')
+        self.assertNotEqual(second_user_list_url, user_list_url)
+        
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Buy milk', page_text)
         
         self.fail('Finish the test!')
