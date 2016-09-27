286d96686a3521f972062630cbcf8bb7313523fe
pawulon <pawelryndak@gmail.com>
2016-09-27 21:57:03 +0200

    Changes in home template and refactoring tests

--- lists/tests.py
+++ lists/tests.py
@@ -39,9 +39,27 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
+    
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        
+        response = home_page(request)
+        
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
-                       
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        request = HttpRequest()
+        response = home_page(request)
+        response = self.remove_csrf_token_from_response(response.content.decode())
+        
+        self.assertIn('itemey 1', response)
+        self.assertIn('itemey 2', response)
 
 class ItemModelTest(TestCase):
     
