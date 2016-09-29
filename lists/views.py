72eeb4cddbe427f983fe6cb83c4941cd8c6c6b67
pawulon <pawelryndak@gmail.com>
2016-09-29 06:32:12 +0200

    new URL /lists/new

--- lists/views.py
+++ lists/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from lists.models import Item
 
-def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')                
-        
+def home_page(request):    
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html',
                  {'items' : items})
+                 
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')                
