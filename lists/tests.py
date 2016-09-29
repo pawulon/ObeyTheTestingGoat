0983cc82618a7f284ee656ba817c8f03453a4087
pawulon <pawelryndak@gmail.com>
2016-09-29 06:47:37 +0200

    Adding new model for Lists

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 import re
 
 class HomePageTest(TestCase):
@@ -39,16 +39,21 @@ class NewListTest(TestCase):
                 data = {'item_text' : 'A new list item'})
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')        
         
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     
-    def save_item(self, item_text):
+    def save_item(self, item_text, list):
         item = Item()
         item.text = item_text
-        item.save()    
+        item.list = list
+        item.save()        
         
     def test_saving_and_retrieving_items(self):
-        self.save_item('The first (ever) list item')
-        self.save_item('Item the second')
+        list_ = List()
+        list_.save()
+        
+        self.save_item('The first (ever) list item', list_)
+        self.save_item('Item the second', list_)
+        
         
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -57,7 +62,9 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
