2a54efde261a24d0f93b33bdce019e36950f7fc1
pawulon <pawelryndak@gmail.com>
2016-09-29 06:14:21 +0200

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -50,17 +50,6 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        
-        request = HttpRequest()
-        response = home_page(request)
-        response = self.remove_csrf_token_from_response(response.content.decode())
-        
-        self.assertIn('itemey 1', response)
-        self.assertIn('itemey 2', response)
-
 class ItemModelTest(TestCase):
     
     def save_item(self, item_text):
@@ -80,4 +69,18 @@ class ItemModelTest(TestCase):
         
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
         
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
