bc0b94e16028bb0a67606570da4f7bcf60e2b685
pawulon <pawelryndak@gmail.com>
2016-10-03 22:05:15 +0200

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,17 +3,33 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import os
+import sys
 
 dir = os.path.dirname(__file__)
 chromedriver_path = os.path.join(dir, 'chromedriver.exe')
 
 class NewVisitorTest(StaticLiveServerTestCase):
     
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+        
     def setUp(self):
         self.browser = webdriver.Chrome(chromedriver_path)
         self.browser.implicitly_wait(3)
     
     def tearDown(self):
+        print('teary leco')
         self.browser.quit()
 
     def check_for_row_in_list_table(self, searched_text):
@@ -22,7 +38,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn(searched_text, [row.text for row in rows])
         
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.assertIn('To-Do', self.browser.title)
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
@@ -42,7 +58,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser.quit()
         self.browser = webdriver.Chrome(chromedriver_path)
         
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -57,15 +73,15 @@ class NewVisitorTest(StaticLiveServerTestCase):
         
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('Buy milk', page_text)               
+        self.assertIn('Buy milk', page_text)                       
         
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
         
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
         inputbox.send_keys('testing\n')
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
         
