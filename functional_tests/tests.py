4390f1f4f21cfa785cd088695acb276881f4c449
pawulon <pawelryndak@gmail.com>
2016-10-03 17:59:55 +0200

    Use Bootstrap to imrpove layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -7,7 +7,7 @@ import os
 dir = os.path.dirname(__file__)
 chromedriver_path = os.path.join(dir, 'chromedriver.exe')
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Chrome(chromedriver_path)
