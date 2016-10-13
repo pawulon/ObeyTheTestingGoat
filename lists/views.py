cab926d0774dbee6bd0791cdc81b40568fa098cd
pawulon <pawelryndak@gmail.com>
2016-10-13 06:40:35 +0200

    Added view test for empty item

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.core.exceptions import ValidationError
 from lists.models import Item, List
 
 def home_page(request):    
@@ -12,7 +13,12 @@ def view_list(request, list_id):
                  
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+    except ValidationError:
+        error = "You can't have an empty list item"       
+        return render(request, 'home.html', {"error" : error})
     return redirect('/lists/%d/' % (list_.id,))
     
 def add_item(request, list_id):
