4e88c4ba38cbe51c835f7e331d27707f668e072c
pawulon <pawelryndak@gmail.com>
2016-10-18 06:54:56 +0200

    POST to the same view

--- lists/views.py
+++ lists/views.py
@@ -7,7 +7,10 @@ def home_page(request):
     return render(request, 'home.html')
 
 def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)    
+    list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
     return render(request, 'list.html',
                  {'list' : list_})
                  
@@ -16,7 +19,9 @@ def new_list(request):
     item = Item.objects.create(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
+        item.save()
     except ValidationError:
+        list_.delete()
         error = "You can't have an empty list item"       
         return render(request, 'home.html', {"error" : error})
     return redirect('/lists/%d/' % (list_.id,))
