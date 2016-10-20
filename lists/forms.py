782157d20432ba1773dcc96908115f68506f6861
pawulon <pawelryndak@gmail.com>
2016-10-20 22:18:11 +0200

    test refactoring

--- lists/forms.py
+++ lists/forms.py
@@ -5,13 +5,18 @@ EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
     
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+    
     class Meta:
         model = Item
         fields = ('text',)
         widgets = {
             'text' : forms.fields.TextInput(attrs={
-            'placeholder' : 'Enter a to-do item',
-            'class' : 'form-control input-lg',
+                'required' : 'required',
+                'placeholder' : 'Enter a to-do item',
+                'class' : 'form-control input-lg',
             }),
         }
         error_messages = {
