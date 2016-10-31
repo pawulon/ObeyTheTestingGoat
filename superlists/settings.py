d4ce2c67de1cb85aee00e5e3958210dd1b8404af
pawulon <pawelryndak@gmail.com>
2016-10-31 16:13:08 +0100

    Custom passwordless auth backend + custom user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -103,3 +103,8 @@ LOGGING = {
     },
     'root' : {'level' : 'INFO'},
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'prmailsender@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
