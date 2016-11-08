6a75a22a0354627317190b2d6019dc5683bb14ce
pawulon <pawelryndak@gmail.com>
2016-11-08 21:15:39 +0100

    Adding key path for auto auth during tests

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -2,6 +2,8 @@ from os import path
 import subprocess
 THIS_FOLDER = path.dirname(path.abspath(__file__))
 
+KEY_PARAM = r'D:\Projekty\AWS\PR_AWS.pem'
+
 def create_session_on_server(host, email):
     return subprocess.check_output(
         [
@@ -9,16 +11,21 @@ def create_session_on_server(host, email):
             'create_session_on_server:email={}'.format(email),
             '--host=pawel@{}'.format(host),
             '--hide=everything,status',
+            '-i',
+            KEY_PARAM
         ],
         cwd=THIS_FOLDER
     ).decode().strip()
         
 def reset_database(host):
+    print('jestem tu: {}'.format(KEY_PARAM))
     subprocess.check_call(
         [
             'fab', 
             'reset_database',
-            '--host=pawel@{}'.format(host)
+            '--host=pawel@{}'.format(host),
+            '-i',
+            KEY_PARAM
         ],
         cwd=THIS_FOLDER
     )
