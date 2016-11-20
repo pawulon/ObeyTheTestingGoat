2bc888b544662eb00756d9026334c5a161a73deb
pawulon <pawelryndak@gmail.com>
2016-11-20 13:31:03 +0100

    new _list view tries to assign owner but cant

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.utils.html import escape
 from lists.views import home_page
+from lists.views import new_list
 from lists.models import Item, List
 from lists.forms import (ExistingListItemForm, ItemForm)
 from lists.forms import (EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR)
@@ -57,6 +58,14 @@ class NewListTest(TestCase):
         self.client.post('/lists/new', data={'text' : ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+        
+    def test_list_owner_is_saved_if_user_is_authenticated(self):
+        request = HttpRequest()
+        request.user = User.objects.create(email='a@b.com')
+        request.POST['text'] = 'new list item'
+        new_list(request)
+        list_ = List.objects.first()
+        self.assertEqual(list_.owner, request.user)
     
 class ListViewTest(TestCase):
 
@@ -146,5 +155,12 @@ class ListViewTest(TestCase):
 class MyListsTest(TestCase):
     
     def test_my_lists_url_renders_my_lists_template(self):
+        User.objects.create(email='a@b.com')
         response = self.client.get('/lists/users/a@b.com/')
         self.assertTemplateUsed(response, 'my_list.html')
+        
+    def test_passes_correct_owner_to_template(self):
+        User.objecs.create(email='wrong@owner.com')
+        correct_user = User.objects.create(email='a@b.com')
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertEqual(response.context['owner'], correct_user)
