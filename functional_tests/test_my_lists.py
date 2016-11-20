b78d2ebbfb8463d859be0f394de9407c4659c6ab
pawulon <pawelryndak@gmail.com>
2016-11-20 13:08:51 +0100

    url, placeholder view, and first-cut templates for my_lists

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -21,9 +21,23 @@ class MyListsTest(FunctionalTest):
                                      
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         email = 'pawel@pawel.com'
-        self.browser.get(self.server_url)
-        self.assert_logged_out(email)
-        
         self.create_pre_authenticated_session(email)
         self.browser.get(self.server_url)
-        self.assert_logged_in(email)
+        self.get_item_input_box().send_keys('Reticulate splines\n')
+        self.get_item_input_box().send_keys('Immanetize eschaton\n')
+        first_list_url = self.browser.current_url
+        
+        self.browser.find_element_by_link_text('My lists').click()        
+        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.assertEqual(self.browser.current_url, first_list_url)
+        
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Click cows\n')
+        second_list_url = self.browser.current_url
+        
+        self.browser.find_element_by_link_text('My lists').click()
+        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.assertEqual(self.browser.current_url, second_list_url)
+        
+        self.browser.find_element_by_link_text('Log out').click()
+        self.assertEqual(self.browser.find_elements_by_link_text('My lists'), [])
